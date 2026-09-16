f71d0d96f5dfeb430c4ca4106d70e7014b5c90b8
juanRodriguex <juan.rodriguez67049@ucaldas.edu.co>
2026-09-15 21:31:41 -0500

    Añadi nuevos objetivos y obstaculos

--- juego.js
+++ juego.js
@@ -1,110 +1,101 @@
-// Primer movimiento
+//Configuración
 
 const config = {
-
-
-type: Phaser.AUTO,
-
-
-width:800,
-
-
-height:600,
-
-backgroundColor:'#0000FF',
-
-scene:{
-
-    create:create,
-    update:update
-
-}
-
-
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#0000FF',
+    scene: {
+        create: create,
+        update: update
+    }
 };
 
+//Variables 
 
 let jugador;
-
 let teclaDerecha;
+let teclaIzquierda;
+let salto;
+let bajar;
+let dash;
 
-let teclaIzquierda
+let objetivo1_elem;
+let objetivo2_elem; // Segundo objetivo
+let obstaculos = [];
 
-let salto
+const POS_INICIAL_X = 400;
+const POS_INICIAL_Y = 300;
 
-let bajar
+//Funciones
 
-let dash
-
-
-function create(){
-
-
-    jugador = this.add.circle(400,300,50,0xff0000);
-
-    objetivo = this.add.circle(
-
-700,
-
-300,
-
-30,
-
-0xffff00
-
-);
-
-
-
-obstaculo =
-
-this.add.rectangle(
-
-500,
-
-300,
-
-50,
-
-150,
-
-0x000000
-);
-    salto = this.input.keyboard.addKey(
-
-        Phaser.Input.Keyboard.KeyCodes.UP
-
-    );
-
-
-     bajar = this.input.keyboard.addKey(
-
-        Phaser.Input.Keyboard.KeyCodes.DOWN
-
-    );
-
-    teclaDerecha = this.input.keyboard.addKey(
-
-        Phaser.Input.Keyboard.KeyCodes.RIGHT
-
-    );
-
-    teclaIzquierda = this.input.keyboard.addKey(
-
-        Phaser.Input.Keyboard.KeyCodes.LEFT
-
-    );
+function create() {
+    jugador = this.add.circle(POS_INICIAL_X, POS_INICIAL_Y, 50, 0xff0000);
     
-    dash = this.input.keyboard.addKey(
+    // Objetivo 1: Círculo amarillo
+    objetivo1_elem = this.add.circle(700, 300, 30, 0xffff00);
+    
+    // Objetivo 2: Cuadrado magenta (Nuevo objetivo)
+    objetivo2_elem = this.add.rectangle(100, 100, 50, 50, 0xff00ff);
 
-        Phaser.Input.Keyboard.KeyCodes.Q
+    // Obstáculo original + 2 nuevos obstáculos
+    obstaculos.push(this.add.rectangle(500, 300, 50, 150, 0x000000));
+    obstaculos.push(this.add.rectangle(250, 150, 60, 120, 0x000000));
+    obstaculos.push(this.add.rectangle(250, 450, 60, 120, 0x000000));
 
-    );
+    salto = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    bajar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    teclaDerecha = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    teclaIzquierda = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    dash = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
 }
 
+// Comprobación de objetivos y colisiones
+
+function verificarObjetivos() {
+    const circuloJugador = new Phaser.Geom.Circle(jugador.x, jugador.y, 50);
+    
+    // Geometría del Objetivo 1 (Círculo)
+    const circuloObjetivo1 = new Phaser.Geom.Circle(objetivo1_elem.x, objetivo1_elem.y, 30);
+
+    // Geometría del Objetivo 2 (Rectángulo)
+    const rectObjetivo2 = new Phaser.Geom.Rectangle(
+        objetivo2_elem.x - objetivo2_elem.width / 2,
+        objetivo2_elem.y - objetivo2_elem.height / 2,
+        objetivo2_elem.width,
+        objetivo2_elem.height
+    );
+
+    // Tocar el Objetivo 1
+    if (Phaser.Geom.Intersects.CircleToCircle(circuloJugador, circuloObjetivo1)) {
+        console.log("¡Has tocado el Objetivo Amarillo!");
+    }
+
+    // Tocar el Objetivo 2 (Nuevo)
+    if (Phaser.Geom.Intersects.CircleToRectangle(circuloJugador, rectObjetivo2)) {
+        console.log("¡Has tocado el Objetivo Magenta!");
+    }
+
+    // Colisión con obstáculos (Perder y reiniciar)
+    for (let obs of obstaculos) {
+        const rectObstaculo = new Phaser.Geom.Rectangle(
+            obs.x - obs.width / 2,
+            obs.y - obs.height / 2,
+            obs.width,
+            obs.height
+        );
+
+        if (Phaser.Geom.Intersects.CircleToRectangle(circuloJugador, rectObstaculo)) {
+            console.log("¡Has tocado un obstáculo! Reiniciando...");
+            jugador.setPosition(POS_INICIAL_X, POS_INICIAL_Y);
+            break;
+        }
+    }
+}
+
+//Ejecución
 
 function update() {
-    
     const velocidad = dash.isDown ? 20 : 5;
 
     if (teclaDerecha.isDown) {
@@ -119,18 +110,11 @@ function update() {
         jugador.y += velocidad;
     }
 
-    jugador.x = Phaser.Math.Clamp(
-jugador.x,
-50,750
-)
+    jugador.x = Phaser.Math.Clamp(jugador.x, 50, 750);
+    jugador.y = Phaser.Math.Clamp(jugador.y, 50, 550);
 
-jugador.y = Phaser.Math.Clamp(
-
-jugador.y,50,550
-
-)
+    verificarObjetivos();
 }
 
-
-
+//Instancia
 const game = new Phaser.Game(config);
